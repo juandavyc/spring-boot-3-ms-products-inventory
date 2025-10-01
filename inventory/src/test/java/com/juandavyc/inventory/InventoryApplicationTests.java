d688790e76cac207d25f835fe23ebcf7275bfdec
juandavyc <davidy0090011@gmail.com>
2025-09-30 21:53:12 -0500

    Initial commit

---
+++ inventory/src/test/java/com/juandavyc/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juandavyc.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
